811cbf364090626f2a1ddf23a9066731aad192fd
Anas mohammad <68404083+AnasMohammad99@users.noreply.github.com>
2023-02-07 17:52:15 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 //const PDFDocument = require ('pdfkit');
 // const fs = require('fs');
 const multer = require('multer');
-// const mysql = require("mysql2");
+const mysql = require("mysql2");
 const app = express();
 const router = express.Router();
 
@@ -19,16 +19,16 @@ const storage = multer.diskStorage({
   })
 
   const upload = multer({ storage: storage })
-// const connection = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "",
-//     database: "test1_db",
-//     });
-//     connection.connect((err) => {
-//         if (err) return console.error(err.message);
-//         console.log("Connected to the MySQL database.");
-//         });
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "test1_db",
+    });
+    connection.connect((err) => {
+        if (err) return console.error(err.message);
+        console.log("Connected to the MySQL database.");
+        });
         
     
     
@@ -36,35 +36,35 @@ const storage = multer.diskStorage({
             if (!req.file) {
               return res.status(400).send('No file uploaded.');
             }
-            // const pdfLink = `uploads/${req.file.filename}`;
-            console.log(req.file)
+            const pdfLink = `uploads/${req.file.filename}`;
+            console.log(pdfLink)
             res.send("file uploaded")
     
     
-            // connection.query(
-            //     'INSERT INTO table_name (pdf_column) VALUES (?)',
-            //     [pdfLink],
-            //     (error, results) => {
-            //       if (error) throw error;
-            //       res.send('PDF uploaded and link stored in database successfully.');
-            //     }
-            //   );
-            // });
+            connection.query(
+                'INSERT INTO table_name (pdf_column) VALUES (?)',
+                [pdfLink],
+                (error, results) => {
+                  if (error) throw error;
+                  res.send('PDF uploaded and link stored in database successfully.');
+                }
+              );
+            });
     
-            // router.get('/download/:pdf_id', (req, res) => {
-            //     connection.query(
-            //       'SELECT pdf_column FROM table_name WHERE id = ?',
-            //       [req.params.pdf_id],
-            //       (error, results) => {
-            //         if (error) throw error;
-            //         if (!results.length) {
-            //           return res.status(404).send('PDF not found.');
-            //         }
+            app.get('/download/:pdf_id', (req, res) => {
+                connection.query(
+                  'SELECT pdf_column FROM table_name WHERE id = ?',
+                  [req.params.pdf_id],
+                  (error, results) => {
+                    if (error) throw error;
+                    if (!results.length) {
+                      return res.status(404).send('PDF not found.');
+                    }
               
-            //         const pdfPath = results[0].pdf_column;
-            //         res.download(pdfPath);
-            //       }
-            //     );
+                    const pdfPath = results[0].pdf_column;
+                    res.download(pdfPath);
+                  }
+                );
               });
               
               app.listen(3000, () => {
